f2a61e7207c77ef2ad6005f3ce06bab0e450343a
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-05-25 00:24:49 +0300

    adding basic styles for mobile screens

--- src/components/PhotoAlbumsList.js
+++ src/components/PhotoAlbumsList.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PhotoAlbum from "./PhotoAlbum";
 import { Link } from "react-router-dom";
 
 export default function PhotoalbumsList({ photoalbums }) {
   return (
-    <div className="photo-album__container">
-      {photoalbums.map((photoAlbum) => {
-        return (
-          <PhotoAlbum
-            key={photoAlbum.id}
-            id={photoAlbum.id}
-            title={photoAlbum.title}
-          />
-        );
-      })}
-      <Link className="btn-favourites" to="/favourites">
-        Show Favourites
-      </Link>
-    </div>
+    <Fragment>
+      <div className="photo-album__container">
+        {photoalbums.map((photoAlbum) => {
+          return (
+            <PhotoAlbum
+              key={photoAlbum.id}
+              id={photoAlbum.id}
+              title={photoAlbum.title}
+            />
+          );
+        })}
+      </div>
+      <div className="btn-favourites__container">
+        <Link className="btn-favourites" to="/favourites">
+          Show Favourites
+        </Link>
+      </div>
+    </Fragment>
   );
 }
